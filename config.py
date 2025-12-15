b6fe8a6a69016caa387a361a5f7da2dc4e06baa4
Htet Khaing Win <p.htetkhaingwin@gmail.com>
2025-12-16 02:06:43 +0800

    Added file path to the config file

--- config.py
+++ config.py
@@ -3,6 +3,8 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+
 api_key = os.getenv('API_KEY')
 project = os.getenv('PROJECT_NAME')
 log = os.getenv('LOG_LEVEL')
