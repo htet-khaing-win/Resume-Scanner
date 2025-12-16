315aea957179f1e4f6ed65cc84242fcb0b54ec91
Htet Khaing Win <p.htetkhaingwin@gmail.com>
2025-12-17 04:23:28 +0800

    Tested the new functions

--- app.py
+++ app.py
@@ -1,12 +1,20 @@
 import os
 from src.pdf_parser import parse_resume_pdf
+from src.text_processor import preprocess_resume, preprocess_job_description
 
 resume_text = parse_resume_pdf("data/raw/Resume.pdf")
+jd_text = parse_resume_pdf("data/raw/JD.txt")
 
-if resume_text:
-    print(f"The extracted text is: {resume_text}")
-    print(f"The length of text is: {len(resume_text)}")
-else:
-    print("Extraction Failed.") 
+resume_data = preprocess_resume(resume_text)
+jd_data = preprocess_job_description(jd_text)
+print(f" \n From Resume: \n {resume_data}")
+print(f" \n From Job Description: \n {jd_data}")
 
+import spacy
+from spacy.lang.en.examples import sentences 
 
+nlp = spacy.load("en_core_web_sm")
+doc = nlp(sentences[0])
+print(doc.text)
+for token in doc:
+    print(token.text, token.pos_, token.dep_)
