37704dc76048f0f471f4207a3ed4b5e1394b6f5e
Htet Khaing Win <p.htetkhaingwin@gmail.com>
2025-12-28 17:48:15 +0800

    Changed File Reading

--- app.py
+++ app.py
@@ -2,19 +2,21 @@ import os
 from src.pdf_parser import parse_resume_pdf
 from src.text_processor import preprocess_resume, preprocess_job_description
 
-resume_text = parse_resume_pdf("data/raw/Resume.pdf")
-jd_text = parse_resume_pdf("data/raw/JD.txt")
-
+# Process Resume
+resume_path = "data/raw/Resume.pdf"
+resume_text = parse_resume_pdf(resume_path)
 resume_data = preprocess_resume(resume_text)
+
+# Process Job Description
+jd_path = "data/raw/JD.txt"
+if os.path.exists(jd_path):
+    with open(jd_path, "r", encoding="utf-8") as f:
+        jd_text = f.read() 
+else:
+    jd_text = ""
+    print("Error: JD.txt not found")
+    
 jd_data = preprocess_job_description(jd_text)
+
 print(f" \n From Resume: \n {resume_data}")
 print(f" \n From Job Description: \n {jd_data}")
-
-import spacy
-from spacy.lang.en.examples import sentences 
-
-nlp = spacy.load("en_core_web_sm")
-doc = nlp(sentences[0])
-print(doc.text)
-for token in doc:
-    print(token.text, token.pos_, token.dep_)
